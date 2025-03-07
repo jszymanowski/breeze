e5b6a7d5dd8bd7ba9054180ba5d12a490c1d5335
John Szymanowski <github@john.szymanow.ski>
2025-03-07 11:10:24 +0800

    Fix storybook

--- packages/primitives/.storybook/main.ts
+++ packages/primitives/.storybook/main.ts
@@ -1,14 +1,12 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-    "@storybook/addon-a11y",
-  ],
+  stories: ["../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
+  core: {
+    builder: "@storybook/builder-vite",
+    disableTelemetry: true,
+  },
   framework: {
     name: "@storybook/react-vite",
     options: {},
