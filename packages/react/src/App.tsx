36165739e0d0a5f92aa23652be4f77cdffb9d0fd
John Szymanowski <github@john.szymanow.ski>
2025-03-10 12:31:46 +0800

    Add variant to stories

--- packages/react/src/App.tsx
+++ packages/react/src/App.tsx
@@ -1,19 +1,7 @@
 import { Demo } from "./Demo";
 
 function App() {
-  return (
-    <div>
-      <h4 className="text-foreground font-display scroll-m-20 text-xl font-bold tracking-normal">
-        User Profile
-      </h4>
-      <h4 className="text-foreground font-display scroll-m-20 text-xl leading-none font-bold tracking-normal">
-        User Profile
-      </h4>
-      <h4 className="text-foreground font-display scroll-m-20 text-xl leading-normal font-bold tracking-normal">
-        User Profile
-      </h4>
-    </div>
-  );
+  return <Demo />;
 }
 
 export default App;
