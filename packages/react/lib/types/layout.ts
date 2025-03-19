ebe8b8fccbea72eb1fe44c26e90ce047b785ce65
John Szymanowski <github@john.szymanow.ski>
2025-03-19 22:59:03 +0800

    Add numeric sizes

--- packages/react/lib/types/layout.ts
+++ packages/react/lib/types/layout.ts
@@ -1,4 +1,43 @@
-export const SIZES = ["auto", "full", "min", "max", "fit", "px"] as const;
+const NUMERIC_SIZES = [
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "11",
+  "12",
+  "16",
+  "20",
+  "24",
+  "28",
+  "32",
+  "36",
+  "40",
+  "44",
+  "48",
+  "52",
+  "56",
+  "60",
+  "64",
+  "72",
+  "84",
+  "96",
+];
+
+export const SIZES = [
+  ...NUMERIC_SIZES,
+  "auto",
+  "full",
+  "min",
+  "max",
+  "fit",
+  "px",
+] as const;
 export type Size = (typeof SIZES)[number];
 
 export const WIDTHS = [
