5968959cc01423339251bd71f15e67dbbc7f0505
John Szymanowski <github@john.szymanow.ski>
2025-04-05 10:56:02 +0800

    Explicitly add htmlFor

--- packages/forms/lib/Label/Label.tsx
+++ packages/forms/lib/Label/Label.tsx
@@ -4,14 +4,16 @@ import { Label as CnLabel } from "@/components/ui/label";
 import { cn } from "@/utils";
 
 export interface LabelProps extends ComponentProps<"label"> {
+  htmlFor?: string;
   value?: string;
   children?: React.ReactNode;
   className?: string;
 }
 
-const Label = ({ value, children, className = "", ...props }: LabelProps) => (
+const Label = ({ htmlFor, value, children, className = "", ...props }: LabelProps) => (
   <CnLabel
     className={cn("font-display font-normal text-base text-foreground", className)}
+    htmlFor={htmlFor}
     {...props}
   >
     {value || children}
--- packages/forms/tests/Label.test.tsx
+++ packages/forms/tests/Label.test.tsx
@@ -7,17 +7,35 @@ describe("Label", () => {
   const EXPECTED_BASE_LABEL_CLASSES =
     "flex select-none items-center gap-2 peer-disabled:cursor-not-allowed peer-disabled:opacity-50 group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 font-display font-normal text-base text-foreground";
 
-  it("renders with default props", () => {
+  it("renders with default props as children", () => {
     render(<Label>Some label</Label>);
-    const element = screen.getByText("Some label");
+    const label = screen.getByText("Some label");
 
-    expect(element.tagName).toBe("LABEL");
-    expect(element.className).toBe(EXPECTED_BASE_LABEL_CLASSES);
+    expect(label.tagName).toBe("LABEL");
+    expect(label.className).toBe(EXPECTED_BASE_LABEL_CLASSES);
+  });
+  
+  it("renders with default props as value", () => {
+    render(<Label value="Some label" />);
+    const label = screen.getByText("Some label");
+
+    expect(label.tagName).toBe("LABEL");
+    expect(label.className).toBe(EXPECTED_BASE_LABEL_CLASSES);
+  });
+  
+  it("renders with htmlFor", () => {
+    render(<Label htmlFor="some-field">Some label</Label>);
+    const label = screen.getByText("Some label") as HTMLLabelElement;
+
+    expect(label.tagName).toBe("LABEL");
+    expect(label.htmlFor).toBe("some-field");
+    expect(label.className).toBe(EXPECTED_BASE_LABEL_CLASSES);
   });
 
   it("combines custom className with generated classes", () => {
     render(<Label className="custom-class">With custom class</Label>);
-    const element = screen.getByText("With custom class");
-    expect(element.className).toBe(`${EXPECTED_BASE_LABEL_CLASSES} custom-class`);
+    
+    const label = screen.getByText("With custom class");
+    expect(label.className).toBe(`${EXPECTED_BASE_LABEL_CLASSES} custom-class`);
   });
 });
