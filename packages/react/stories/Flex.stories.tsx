096a29e4b2d30eb5737b559753a7b1f05e94ec32
John Szymanowski <github@john.szymanow.ski>
2025-04-05 21:28:29 +0800

    Fix default Flex story

--- packages/react/stories/Flex.stories.tsx
+++ packages/react/stories/Flex.stories.tsx
@@ -135,15 +135,9 @@ export const Default: Story = {
     className: "w-full h-full border-1 border-dashed border-gray-300",
     children: (
       <Fragment>
-        <FlexItem>
-          <Text variant="info">Item 1</Text>
-        </FlexItem>
-        <FlexItem>
-          <Text variant="info">Item 2</Text>
-        </FlexItem>
-        <FlexItem>
-          <Text variant="info">Item 3</Text>
-        </FlexItem>
+        <FlexItem>Item 1</FlexItem>
+        <FlexItem>Item 2</FlexItem>
+        <FlexItem>Item 3</FlexItem>
       </Fragment>
     ),
   },
