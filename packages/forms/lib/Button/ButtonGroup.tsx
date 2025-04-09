3492c012dfe0d20fa22227581c167b36f69ad011
John Szymanowski <github@john.szymanow.ski>
2025-04-09 22:32:02 +0800

    Support adding classNames to ButtonGroup.Button

--- packages/forms/lib/Button/ButtonGroup.tsx
+++ packages/forms/lib/Button/ButtonGroup.tsx
@@ -1,6 +1,9 @@
 import { Flex } from "@jszymanowski/breeze-primitives";
 import type React from "react";
+
 import { Button, type ButtonProps } from "./Button";
+
+import { cn } from "@/utils";
 
 export interface ButtonGroupProps {
   children: React.ReactNode;
@@ -11,9 +14,9 @@ export const ButtonGroup = ({ children, className = "" }: ButtonGroupProps) => (
   <Flex className={className}>{children}</Flex>
 );
 
-ButtonGroup.Button = ({ children, ...props }: ButtonProps) => {
+ButtonGroup.Button = ({ children, className, ...props }: ButtonProps) => {
   return (
-    <Button className="not-first:-ml-1 not-last:rounded-r-none not-first:rounded-l-none" {...props}>
+    <Button className={cn("not-first:-ml-1 not-last:rounded-r-none not-first:rounded-l-none", className)} {...props}>
       {children}
     </Button>
   );
